73a0d43f51bcc6352a10a12407f6a2a6da9937ab
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-19 01:29:20 -0500

    Update preprocessor.js

--- preprocessor.js
+++ preprocessor.js
@@ -6,8 +6,8 @@ function changeContractions(string) {
     string = string.split(' ');
     for (i = 0; i < string.length; i++) {
         for (j = 0; j < contractions.length; j++) {
-            string[i] = string[i].toLowerCase().replace(/'\B|[^a-z'? ]/g, ``);
-            if (contractions[j][0] === string[i]) {
+            let test = string[i].toLowerCase().replace(/'\B|[^a-z'? ]/g, ``);
+            if (contractions[j][0] === test) {
                 string[i] = contractions[j][1];
             }
         }
