02efa4f1574657aba609b5ce23d425fc4924cc1d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-19 01:25:30 -0500

    Update preprocessor.js

--- preprocessor.js
+++ preprocessor.js
@@ -2,9 +2,23 @@ function setOutput(string) {
     $('#output').text(string);
 }
 
+function changeContractions(string) {
+    string = string.split(' ');
+    for (i = 0; i < string.length; i++) {
+        for (j = 0; j < contractions.length; j++) {
+            string[i] = string[i].toLowerCase().replace(/'\B|[^a-z'? ]/g, ``);
+            if (contractions[j][0] === string[i]) {
+                string[i] = contractions[j][1];
+            }
+        }
+    }
+    string = string.join(' ');
+    setOutput(string);
+}
+
 function removeAccents(string) {
     string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    setOutput(string);
+    changeContractions(string);
 }
 
 $(document).ready(function () {
