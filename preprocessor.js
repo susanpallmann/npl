ffc1f1af37ad2d40478a6943a47a8be584277638
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-18 22:06:10 -0500

    Update preprocessor.js

--- preprocessor.js
+++ preprocessor.js
@@ -2,11 +2,8 @@ function setOutput(output) {
     $('#output').text(output);
 }
 
-$('document').ready(function() {
-    $('#input').on('keyup change paste', 'input, select, textarea', function(){
-        console.log("This ran.");
-        setOutput($('#input').text());
-    });
+$('#input').on('keyup change paste', 'input, select, textarea', function(){
+    console.log("This ran.");
+    setOutput($('#input').text());
 });
-
 // Magic comment
