57368152af791ac40155a4f4b3d59130c491693f
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-18 22:04:25 -0500

    Update preprocessor.js

--- preprocessor.js
+++ preprocessor.js
@@ -2,7 +2,7 @@ function setOutput(output) {
     $('#output').text(output);
 }
 
-$(document).ready(function() {
+$('document').ready(function() {
     $('#input').on('keyup change paste', 'input, select, textarea', function(){
         console.log("This ran.");
         setOutput($('#input').text());
