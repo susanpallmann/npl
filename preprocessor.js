765d1115268e05beaa1ecb5fdff004187193c825
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-18 22:16:45 -0500

    Update preprocessor.js

--- preprocessor.js
+++ preprocessor.js
@@ -1,13 +1,9 @@
-function setOutput(output) {
-    $('#output').text(output);
-}
-
-$('#input').on('input', function() {
-    setOutput($('#input').text());
-});
-
 $(document).ready(function () {
     console.log("This ran.");
+    
+    function setOutput(output) {
+        $('#output').text(output);
+    }
     
     $('#input').on('input', function() {
         console.log("This ran.");
