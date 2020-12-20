b264a8810187200ba7f677c22ac7ac7302b3fc44
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-19 22:05:16 -0500

    Create vocabulary.js

---
+++ vocabulary.js
@@ -0,0 +1,94 @@
+let contractions = [
+    ["can't", "can not"],
+    ["won't", "will not"],
+    ["wouldn't", "would not"],
+    ["couldn't", "could not"],
+    ["aren't", "are not"],
+    ["didn't", "did not"],
+    ["don't", "do not"],
+    ["doesn't", "does not"],
+    ["hadn't", "had not"],
+    ["haven't", "have not"],
+    ["he's", "he is"],
+    ["he'll", "he will"],
+    ["he'd", "he would"],
+    ["here's", "here is"],
+    ["I'm", "I am"],
+    ["I've", "I have"],
+    ["I'll", "I will"],
+    ["I'd", "I would"],
+    ["isn't", "is not"],
+    ["it's", "it is"],
+    ["it'll", "it will"],
+    ["mustn't", "must not"],
+    ["she's", "she is"],
+    ["she'll", "she will"],
+    ["she'd", "she would"]
+];
+
+// Stative Verbs
+let mentalStateVerbs = [
+    "know",
+    "believe",
+    "understand",
+    "doubt",
+    "think",
+    "suppose",
+    "recognise",
+    "forget",
+    "remember",
+    "imagine",
+    "mean",
+    "agree",
+    "disagree",
+    "deny",
+    "promise",
+    "satisfy",
+    "realise",
+    "appear",
+    "astonish",
+    "please",
+    "impress",
+    "surprise",
+    "concern"
+];
+
+let possessionVerbs = [
+    "have",
+    "own",
+    "possess",
+    "lack",
+    "consist",
+    "involve",
+    "include",
+    "contain"
+];
+
+let emotionsVerbs = [
+    "love",
+    "like",
+    "dislike",
+    "hate",
+    "adore",
+    "prefer",
+    "care for",
+    "mind",
+    "want",
+    "need",
+    "desire",
+    "wish",
+    "hope",
+    "appreciate",
+    "value"
+];
+
+let measureVerbs = [
+    "cost",
+    "measure",
+    "weigh",
+    "owe",
+    "seem",
+    "fit",
+    "depend",
+    "matter"
+];
